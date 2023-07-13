0abfdb98525c2d742fc7e5b0733fb68fa2aacaa9
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-13 07:53:31 -0700

    .

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
     import sys
 
-    args = len(argv) - 1
+    argc = len(argv) - 1
     res = 0
-    i = 1
 
-    for i in range(args):
-        res += int(sys.argv[i])
+    for i in range(argc):
+        res += int(sys.argv[i + 1])
 
     print("{}".format(res))
