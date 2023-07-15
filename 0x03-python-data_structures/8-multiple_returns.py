92c34678d49ee3859be77c938df442373a1fdb48
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-15 06:49:59 -0700

    .

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     str_tuple = ()
-    if len(sentence) == 0:
-        str_tuple= "0", "None"
+    if sentence == '':
+        str_tuple = "0", "None"
     else:
         str_tuple = len(sentence), sentence[0]
     return str_tuple
