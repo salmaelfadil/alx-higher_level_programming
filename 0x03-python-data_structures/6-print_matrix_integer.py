6c13d3ebe6408a15258fbfd0adc2ab5df956ea78
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-15 06:29:10 -0700

    .

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in matrix:
-            print("{}".format(j), end="" if j != i[-1] else "")
-        print()
+        for j in i:
+            if j != i[-1]:
+                print("{:d}".format(j), end=" ")
+            else:
+                print("{:d}".format(j))
