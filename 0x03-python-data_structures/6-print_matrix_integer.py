6723bf10c7d01dd102546f36f27f9ccf8b55fdd5
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-15 06:44:06 -0700

    .

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             if j != i[-1]:
                 print("{:d}".format(j), end=" ")
             else:
-                print("{:d}".format(j), end="")
+                print("{:d}".format(j))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple = ()
-    a = tuple_a([0]) + tuple_b([0]) + (0, 0)
-    b = tuple_a([1]) + tuple_b([1]) + (0, 0)
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    a = tuple_1[0] + tuple_2[0]
+    b = tuple_1[1] + tuple_2[1]
     new_tuple = a, b
     return new_tuple
