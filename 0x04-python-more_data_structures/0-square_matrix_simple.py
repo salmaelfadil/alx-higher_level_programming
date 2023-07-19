f67ab2819eab2d48b53f68141f25a0bd44847d53
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-19 05:49:58 -0700

    .

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map((lambda x: x**2, i)) for i in matrix)
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: x**2, x)))
     return new_matrix
