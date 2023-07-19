a4c79fcdc8882854590de000a4dedfcd94cd1ba1
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-19 08:00:39 -0700

    .

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary == {} or a_dictionary is None:
         return None
     else:
-        new_list = list(a_dictionary.key())
+        new_list = list(a_dictionary.keys())
         largest = 0
         l_key = ""
         for i in new_list:
