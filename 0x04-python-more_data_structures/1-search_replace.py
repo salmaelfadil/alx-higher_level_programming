a212563f23c4b0a0ccc9e7d53b9560cb76e71736
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-19 06:24:53 -0700

    .

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i0
+    return new_list
