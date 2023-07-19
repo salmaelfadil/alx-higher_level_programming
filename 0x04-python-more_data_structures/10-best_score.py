92a29a161181f6e37d424c126aad06675d374137
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-19 07:59:51 -0700

    .

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary = {} or a_dictionary is None:
+    if a_dictionary == {} or a_dictionary is None:
         return None
     else:
         new_list = list(a_dictionary.key())
