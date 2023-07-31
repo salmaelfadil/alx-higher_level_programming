52b2cb8f8c04eb39b57fb4b3afd4dc3a35013236
salma elfadil <salmaelfadil@aucegypt.edu>
2023-07-31 06:14:52 -0700

    .

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in list_length:
+    for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
-        except (ZeroDivisionError):
+        except ZeroDivisionError:
             print("division by 0")
             div = 0
         except TypeError:
