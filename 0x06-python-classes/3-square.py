ad091acc66a89e1ee1fa9b3b6abac367c223c6c7
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-01 15:29:00 -0700

    .

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     """square class with init size and validation and area"""
 
     def __init__(self, size=0):
-        if type(size) is int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
