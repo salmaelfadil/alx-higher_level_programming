f72a0bbc46e364ff5f3684df750d6afcb7af020d
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-07 14:49:26 -0700

    .

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,8 @@
 
 
 class Rectangle:
-    """Represent a Rectangle."""
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         """Initializes the rectagnle"""
         self.width = width
@@ -12,12 +13,12 @@ class Rectangle:
 
     @property
     def width(self):
-        """ width getter method"""
+        """width getter method"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ width setter method """
+        """width setter method """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,7 +27,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """ height getter method"""
+        """height getter method"""
         return self.__height
 
     @height.setter
