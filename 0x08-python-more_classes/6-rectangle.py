1aaf9bd82a3d32503f5534559f6694c571ec831a
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-07 08:42:10 -0700

    .

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """Initializes the rectagnle"""
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
