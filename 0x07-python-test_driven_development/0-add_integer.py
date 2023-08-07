b8429280df98d79d1730a456e4230d1bcec977e5
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-07 05:14:44 -0700

    .

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,15 +3,14 @@
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers
+    """ Adds two integers
     Args:
     @a: first int
-    @b: second int, default = 98
+    @b: second int, defaults to 98
     Return: added int
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int or type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
