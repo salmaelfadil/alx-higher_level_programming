e8842e785870aa2a146da57a0cf878e66cff2ffc
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-07 15:04:49 -0700

    .

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
-
+i
 
 class Rectangle:
     """Represent a rectangle."""
@@ -37,4 +37,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
