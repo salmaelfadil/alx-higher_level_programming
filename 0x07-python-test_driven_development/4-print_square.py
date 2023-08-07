4ca06ce9040f2ec58f832a2977a93fd1ae2af88f
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-07 06:02:48 -0700

    .

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""function print_square prints a square with size provided"""
+
+def print_square(size):
+    """
+    prints a square using '#' depending on size provided
+    Args:
+        @size: size of square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        return
+    for i in range(size):
+        print("#" * size)
