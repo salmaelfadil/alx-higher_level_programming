19d45e73ea037c5d82cd3c19236371ea0831bf15
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-14 04:34:14 -0700

    .

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area of rectangle"""
+        return self.__width * self.__height
