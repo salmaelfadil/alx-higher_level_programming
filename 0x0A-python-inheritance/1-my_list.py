976e4eb224b8a551732c3ab1e230b9d559d3ef64
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-14 04:10:30 -0700

    .

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,10 @@
 
 class MyList(list):
     """inherits from list"""
+    def __init__(self):
+        """initalizes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """sorts a list"""
         print(sorted(self))
