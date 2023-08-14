d477101b6300e2c5ec6bf6520c80804bbc1a5d02
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-14 04:25:49 -0700

    .

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class with public attribute area"""
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
