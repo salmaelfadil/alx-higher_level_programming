d78a8600e1c94ce9eba6eb4f0b45fa0e403d78b9
salma elfadil <salmaelfadil@aucegypt.edu>
2023-08-15 04:46:16 -0700

    .

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class student"""
+
+
+class Student:
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initalizes a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrives a dictrionary representation of student"""
+        if attrs is None:
+            return self.__dict__
+        json_dict = {}
+
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_dict[attr] = getattr(self, attr)
+        return json_dict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
