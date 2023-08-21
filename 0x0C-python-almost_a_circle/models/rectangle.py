46eb482ab45c897190f677528913a19c23a2d47b
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-08-21 16:41:02 -0700

    .

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,6 +101,6 @@ class Rectangle(Base):
         rec_dict = {}
 
         for attr in attrs:
-            dict_res[key] = getattr(self, key)
+            rec_dict[key] = getattr(self, key)
 
         return rec_dict
