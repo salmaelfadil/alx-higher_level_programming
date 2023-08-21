c45e49ba7647db449d752fd8f573e92d04c3c07d
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-08-21 13:12:50 -0700

    .

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a Rectangle"""
+    def __init__(self, x, y, width, height, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
