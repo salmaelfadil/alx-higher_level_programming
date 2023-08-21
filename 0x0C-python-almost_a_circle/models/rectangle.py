9f2a0eda5b3b7cb99e4c512628c14717df407ff7
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-08-21 16:08:11 -0700

    .

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,14 @@ class Rectangle(Base):
                 self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args):
-        attrs = ["id", "__width", "__height", "__x", "__y"]
-        for i, arg in enumerate(args):
-            setattr(self, attrs[i], arg)
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) >= 5:
+            self.__y = args[4]
 
