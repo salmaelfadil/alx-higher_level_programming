e67fa9d1f149a28bfb50e4ffe79a9dbd0c66337c
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-08-21 16:47:41 -0700

    .

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,3 +12,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @static.method
+    def to_json_string(list_dictionaries):
+        """returns json string representation of dicts"""
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
