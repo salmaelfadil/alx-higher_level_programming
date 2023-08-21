e92efee777ead7baa03ef3441f1167bfe4a5a42c
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-08-21 13:03:49 -0700

    .

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+"""Base class"""
+
+
+class Base:
+    """defines a base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        __nb_objects += 1
+        self.id = Base.__nb.objects
