7bed8cbbdb495dd3056d19a2bff1c4f0bf58c9ae
Salma ElFadil <salmelfadil@aucegypt.edu>
2023-10-17 09:42:29 -0700

    .

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let count = 0;
-	for (let i = 0; i < list.length; i++) {
-		if (searchElement == list[i]) {
-		count++
-		}
-	}
-	return count;
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement == list[i]) {
+      count++;
+    }
+  }
+  return count;
 };
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-	const rev = [];
-	const elem = list.length - 1;
-	for (let i = last; i >=0; i--) {
-		rev.push(list[i]);
-	}
-	return rev;
-}
+  const rev = [];
+  const elem = list.length - 1;
+  for (let i = elem; i >= 0; i--) {
+    rev.push(list[i]);
+  }
+  return rev;
+};
