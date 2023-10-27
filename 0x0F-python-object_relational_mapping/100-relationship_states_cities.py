9be0418043d98c2ab6de6ab44261d6f6545dd735
salma elfadil <salmaelfadil@aucegypt.edu>
2023-10-27 03:03:08 -0700

    .

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,7 +7,7 @@ import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine(
