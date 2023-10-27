06cbd8035cf6267f67b5a5e695d921ecad7e879f
salma elfadil <salmaelfadil@aucegypt.edu>
2023-10-26 23:49:27 -0700

    .

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
 import sys
 import MySQLdb
-
-"""lists all states from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
