851655132dc8bc66d4bfb0553d4c2cc763ec985d
salma elfadil <salmaelfadil@aucegypt.edu>
2023-10-27 02:36:02 -0700

    .

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-python file that contains the class definition of a City
+"""python file that contains the class definition of a City
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from model_state import Base
