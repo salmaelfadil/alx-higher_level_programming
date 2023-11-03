4e701ead8a1dbe6314086f5c764f8f49259c431e
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 02:20:40 -0700

    .

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf content: {}".format(html.decode("utf-8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf content: {}".format(html.decode("utf-8")))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes a URL, sends a request and displays the body of response"""
+import urllib.request
+import sys
+import urllib.error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
