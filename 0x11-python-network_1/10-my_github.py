902140bb3d237faa11dd8dbf5c2931d641ea7b6c
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 03:15:05 -0700

    .

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""takes your GitHub credentials and uses GitHub API to display your id"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    auth = HTTPBasicAuth(username, access_token)
+    request = requests.get(url, auth=auth)
+    print(request.json().get("id"))
