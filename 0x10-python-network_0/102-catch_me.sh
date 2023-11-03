88498d159290ae9f17f78d8285890391c3902d8e
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 01:07:17 -0700

    .

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me
-# server to respond with a message containing You got me!
+# makes a request to server to respond with a message
 curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
