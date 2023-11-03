84e70d6fd48c5e8abb064315a2a9cb7b7eae2ed5
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 02:36:16 -0700

    .

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""fetches a url and sends a request, display response"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    
+    html = requests.get(url)
+    if html.status_code >= 400:
+        print("Error code: {}".format(html.status_code))
+    else:
+        print(html.text)
