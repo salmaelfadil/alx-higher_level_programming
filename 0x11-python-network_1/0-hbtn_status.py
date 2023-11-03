172831e69bd016f2d676c4d7297b77131ed6b76b
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 03:28:06 -0700

    .

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,8 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as res:
         html = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(html)))
