ed57e17678097f39af1fc58815cb0d25c2573ccb
salmaelfadil <salmaelfadil@aucegypt.edu>
2023-11-03 02:26:42 -0700

    .

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""fetches a url using requests"""
+import requests
+
+if __name__ == "__main__":
+    html = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
